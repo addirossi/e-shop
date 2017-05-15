6178860d1e7e29933088bb000edf4600e66a82b0
addirossi <addirossi@gmail.com>
2017-05-15 18:22:53 +0600

    Cart auto refreshing added

--- shop/cart/views.py
+++ shop/cart/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from shop.catalog.models import Product
@@ -17,6 +18,21 @@ def cart_add(request, product_id):
         cd = form.cleaned_data
         cart.add(product=product, quantity=cd['quantity'], update_quantity=cd['update'])
     return redirect('cart:cart-detail')
+
+
+def cart_add_2(request, product_id):
+    res = {'status': 'ok', 'redirect': True}
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], update_quantity=cd['update'])
+        if cd['update']:
+            res['redirect'] = False
+    else:
+        res['status'] = 'fail'
+    return JsonResponse(res)
 
 
 def cart_remove(request, product_id):
